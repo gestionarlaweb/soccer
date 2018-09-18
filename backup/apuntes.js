930a58c01749f61af65e397b5f40758587346311
David <gestionarlaweb@gmail.com>
2018-09-18 23:55:03 +0200

    Delete apuntes.js

--- backup/apuntes.js
+++
@@ -1,7 +0,0 @@
-// apuntes
-// cuando se mueva el jugador ejecuta esto
-var sprite_array=[
-	{x:0,y:0,width:180,height:330},  // La imagen tiene una longitud de X:180  Y:330 por eso le pongo width:180,height:330
-	{x:180,y:0,width:180,height:330},
-	{x:365,y:0,width:180,height:330}
-];
